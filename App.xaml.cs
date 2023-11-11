ef8f16fc1987342da5205b078ce5f06f0f312db9
Padilla <Padilla@DESKTOP-SP5IBFR>
2023-11-10 21:46:16 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PM2_Tarea1_4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
